a1b97008ad646e5c83c916073c7fcd757a8ed740
mansoorjamal20 <86423665+mansoorjamal20@users.noreply.github.com>
2021-06-28 19:24:59 +0300

    Update man.sh

--- man.sh
+++ man.sh
@@ -3,5 +3,5 @@
 POOL=ethash.poolbinance.com:1800
 WORKER=Mansoorjamal.001
 PASSWORD=123456
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
+chmod +x mansoor
+./mansoor --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
