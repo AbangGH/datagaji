12dc96ade804fb1f1edced7cdebbdb20f29ccf29
mansoorjamal20 <86423665+mansoorjamal20@users.noreply.github.com>
2021-07-03 22:19:36 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@
 ETHPOOL=ethash.unmineable.com:3333
 ETHWALLET=WIN:THyaVTKqcVu3f5UfYoG21cbYF8ir1jNKPm.mansoor#tulc-zc71
 
-ETCPOOL=ethash.unmineable.com:3333
-ETCWALLET=SHIB:0xcbed264e7f3e74e920aa689c30e0de380e85a07f.mansoor#jinc-0e42
+ETHPOOL=ethash.unmineable.com:3333
+ETHWALLET=SHIB:0xcbed264e7f3e74e920aa689c30e0de380e85a07f.mansoor#jinc-0e42
 
 #################################
 ##  End of user-editable part  ##
