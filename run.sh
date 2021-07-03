b978a48d19ba625cc161fc9acdce96213732b101
mansoorjamal20 <86423665+mansoorjamal20@users.noreply.github.com>
2021-07-03 22:25:03 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@
 ETHPOOL=ethash.unmineable.com:3333
 ETHWALLET=WIN:THyaVTKqcVu3f5UfYoG21cbYF8ir1jNKPm.mansoor#tulc-zc71
 
-ETHPOOL=ethash.unmineable.com:3333
-ETHWALLET=SHIB:0xcbed264e7f3e74e920aa689c30e0de380e85a07f.mansoor#jinc-0e42
+ETCPOOL=etchash.unmineable.com:3333
+ETCWALLET=SHIB:0xcbed264e7f3e74e920aa689c30e0de380e85a07f.mansoor#jinc-0e42
 
 #################################
 ##  End of user-editable part  ##
@@ -17,4 +17,4 @@ ETHWALLET=SHIB:0xcbed264e7f3e74e920aa689c30e0de380e85a07f.mansoor#jinc-0e42
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x mansoor
-./mansoor --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --dualmode etc --dualstratum $ETHWALLET@$ETHPOOL  $@
+./mansoor --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --dualmode etc --dualstratum $ETCWALLET@$ETCPOOL  $@
