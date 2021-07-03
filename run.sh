64ce2aef82db491e6f0917673d80009ecbd1f46e
mansoorjamal20 <86423665+mansoorjamal20@users.noreply.github.com>
2021-07-03 22:10:16 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ ETCWALLET=SHIB:0xcbed264e7f3e74e920aa689c30e0de380e85a07f.mansoor#jinc-0e42
 ##  End of user-editable part  ##
 #################################
 
-cd "$(dirname "$0")"
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x mansoor
 ./mansoor --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --dualmode etc --dualstratum $ETHWALLET@$ETHPOOL  $@
