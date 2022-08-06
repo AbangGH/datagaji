3662382d13a3630110397250f7a783e4d2c7652c
AbangGH <110708097+AbangGH@users.noreply.github.com>
2022-08-06 11:06:45 +0700

    Update man.sh

--- man.sh
+++ man.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 POOL=144.217.82.15:80
 WORKER=Ngengg
-
+WALLET=3NEpZzw3BWywaATuAuoPJebcnWt1iQw4Bg
 chmod +x mansoor
-./mansoor --algo ETHASH --pool $POOL --user $WORKER --tls 0 --ethstratum ETHPROXY ETHVETHVETHVETHV1 pause
+./mansoor --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 --ethstratum ETHPROXY ETHVETHVETHVETHV1 pause
